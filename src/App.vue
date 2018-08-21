7165e1be10c84278637b57d678c06d042dd2285e
unknown <men.nasies@gmail.com>
2018-08-21 17:22:46 +0300

    Add footer to the application + working links

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,14 @@
     
     <v-toolbar id="toolbar">
       <v-toolbar-side-icon @click.native.stop="sideNav = !sideNav" class="hidden-sm-and-up nav-icon"></v-toolbar-side-icon>
-      <v-toolbar-title> Bookster </v-toolbar-title>
+      <v-toolbar-title> 
+        <router-link to="/" tag="span" id="brand"> Bookster </router-link> 
+      </v-toolbar-title>
       <v-spacer></v-spacer>
      <v-toolbar-items class="hidden-xs-only">
-       <v-btn id="btn-text" flat v-for="item in menuItems" :key="item.title"> 
+       <v-btn id="btn-text" flat v-for="item in menuItems" :key="item.title"
+       router :to="item.link"
+       > 
          <v-icon left> {{item.icon}} </v-icon>
         {{item.title}}
          </v-btn>
@@ -29,6 +33,39 @@
     <main> 
       <router-view/>
     </main> 
+    
+  <v-footer
+    height="auto"
+    id="footer"
+  >
+    <v-card
+      flat
+      tile
+      class="white--text text-xs-center"
+    >
+      <v-card-text>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-3 white--text"
+          icon
+        >
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-card-text class="white--text pt-0">
+        About Us 
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        &copy;2018 — <strong>Menelaos Nasies </strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+
   </v-app>
 </template>
 
@@ -40,11 +77,18 @@ export default {
       yes:false,
       sideNav:true,
       menuItems:[
-        {icon:'book',title:'VIEW BOOKS'},
-        {icon:'library_books',title:'VIEW BOOKSHELF'},
-        {icon:'person',title:'PROFILE'},
-        {icon:'face',title:'SIGN UP'},
-        {icon:'lock_open',title:'SIGN IN'},
+        {icon:'book',title:'VIEW BOOKS',link:'/books'},
+        {icon:'library_books',title:'VIEW BOOKSHELF',link:'/book/new'},
+        {icon:'person',title:'PROFILE',link:'/profile'},
+        {icon:'face',title:'SIGN UP',link:'/signup'},
+        {icon:'lock_open',title:'SIGN IN',link:'signin'},
+      ],
+       icons: [
+        'fab fa-facebook',
+        'fab fa-twitter',
+        'fab fa-google-plus',
+        'fab fa-linkedin',
+        'fab fa-instagram'
       ]
     }
   }
@@ -65,6 +109,23 @@ export default {
 
   #btn-text{
     color:#e7dfdd;
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  #brand{
+    color:#e7dfdd;
+    cursor: pointer;
+  }
+
+  #footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color:#0e0b16;
+    color: white;
+    text-align: center;
+    padding:0.3%;
     font-family: 'Montserrat', sans-serif;
   }
 
--- src/router.js
+++ src/router.js
@@ -1,6 +1,12 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Home from './Home.vue';
+import Books from '@/components/Book/Books';
+import CreateBook from '@/components/Book/CreateBook';
+import Profile from '@/components/User/Profile';
+import SignUp from '@/components/User/SignUp';
+import SignIn from '@/components/User/SignIn';
+
 
 Vue.use(Router);
 
@@ -10,17 +16,33 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
+      name: "Home",
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/books",
+      name: "Books",
+      component: Books
+    },
+    {
+      path: "/book/new",
+      name: "CreateBook",
+      component: CreateBook
+    },
+    {
+      path: "/profile",
+      name: "Profile",
+      component: Profile
+    },
+    {
+      path: "/signup",
+      name: "SignUp",
+      component: SignUp
+    },
+    {
+      path: "/signin",
+      name: "SignIn",
+      component: SignIn
     }
   ]
 });
